b6b905a51ac65603971a89db4983b9373f7c8c8f
Sergey Kuzminich <SergeyKuzminich@yandex.ru>
2020-09-09 21:25:03 +0300

    Configuration exception fix

--- src/Configurations/FirebaseConfigurations.php
+++ src/Configurations/FirebaseConfigurations.php
@@ -1,8 +1,10 @@
 <?php
 namespace CarloNicora\Minimalism\Services\Firebase\Configurations;
 
+use CarloNicora\Minimalism\Core\Events\MinimalismErrorEvents;
 use CarloNicora\Minimalism\Core\Services\Abstracts\AbstractServiceConfigurations;
 use CarloNicora\Minimalism\Core\Services\Exceptions\ConfigurationException;
+use Exception;
 
 class FirebaseConfigurations extends AbstractServiceConfigurations {
     /** @var string  */
@@ -14,10 +16,12 @@ class FirebaseConfigurations extends AbstractServiceConfigurations {
     /**
      * firebaseConfigurations constructor.
      * @throws ConfigurationException
+     * @throws Exception
      */
     public function __construct() {
         if (!($this->key = getenv('MINIMALISM_SERVICES_FIREBASE_KEY'))) {
-            throw new ConfigurationException('firebase', 'MINIMALISM_SERVICES_FIREBASE_KEY is a required configuration');
+            MinimalismErrorEvents::CONFIGURATION_ERROR('MINIMALISM_SERVICES_FIREBASE_KEY is a required configuration')
+            ->throw(ConfigurationException::class);
         }
 
         if (!($this->url = getenv('MINIMALISM_SERVICES_FIREBASE_URL'))) {
